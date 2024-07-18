0f9fe93d8d57d7c1c15c6c9577a61db1e7ac4637
brendahuwitonze <uwitonzebrendah@gmail.com>
2024-07-18 12:07:19 +0200

    styling the nav and main page

--- src/header.jsx
+++ src/header.jsx
@@ -4,14 +4,13 @@ import logo from "../src/assets/react.svg"
  export default function Header(){
     return(
     <div>
-    <header>
       <nav>
         <div className="navBar">
         <img src={logo} alt=""/>
         <h1 className="h1Raeact">ReactFacts</h1>
+        <h2>React Course - Project 1</h2>
         </div>
       </nav>
-    </header>
    </div>
     )
  }
